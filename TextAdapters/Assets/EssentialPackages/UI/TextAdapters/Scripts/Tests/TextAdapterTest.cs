f68ce722396b33e604f8318c563d1647201aa0a9
Lars Wobus <lars.wobus@web.de>
2018-11-18 12:50:41 +0100

    Unit tests for TextAdapter.cs

---
+++ TextAdapters/Assets/EssentialPackages/UI/TextAdapters/Scripts/Tests/TextAdapterTest.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using EssentialPackages.UI.TextAdapters.Interfaces;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+using Object = UnityEngine.Object;
+
+namespace EssentialPackages.UI.TextAdapters.Tests
+{
+    public class TextAdapterTest
+    {
+        private GameObject DummyGameObject { get; set; }
+        private ITextComponent TargetScript { get; set; }
+        private Text TextComponent { get; set; }
+        private string Id { get; set; }
+        private string TextContent { get; set; }
+        
+        private void AttachTargetScriptToDummyGameObject()
+        {
+            TargetScript = DummyGameObject.AddComponent<TextAdapter>();
+            // When working in the editor, this serialized field would be initialized with an empty string
+            TargetScript.Id = "";
+        }
+        
+        private void AttachTextComponentToDummyGameObject()
+        {
+            TextComponent = DummyGameObject.AddComponent<Text>();
+            // When working in the editor, this serialized field would be initialized with an empty string
+            TextComponent.text = "";
+            
+            var type = typeof(TextAdapter);
+            var fieldInfo = type.GetField("_text", BindingFlags.NonPublic | BindingFlags.Instance);
+            fieldInfo?.SetValue(TargetScript, TextComponent);
+        }
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            Id = "0";
+            TextContent = "Lorem Ipsum";
+        }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            var typeName = GetType().ToString();
+            DummyGameObject = new GameObject(typeName);
+            AttachTargetScriptToDummyGameObject();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            TextComponent = null;
+            Object.Destroy(DummyGameObject);
+        }
+
+        [UnityTest]
+        public IEnumerator GetId_Should_ReturnEmptyString_When_IdWasNotSetBefore()
+        {
+            Assert.IsEmpty(TargetScript.Id);
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator GetId_Should_ReturnNonEmptyString_When_SetIdWasCalledBefore()
+        {
+            TargetScript.Id = Id;
+            Assert.AreEqual(TargetScript.Id, Id);
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator GetText_Should_ReturnNull_When_TextComponentWasNotConnectedBefore()
+        {
+            var text = TargetScript.Text;
+            Assert.Pass();
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator SetText_Should_NotChangeAnything_When_TextComponentWasNotConnectedBefore()
+        {
+            TargetScript.Text = TextContent;
+            Assert.Pass();
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator GetText_Should_ReturnTextContent_When_TextComponentWasConnectedBefore()
+        {
+            AttachTextComponentToDummyGameObject();
+            TextComponent.text = TextContent;
+            
+            var text = TargetScript.Text;
+            
+            Assert.AreEqual(text, TextContent);
+            yield return null;
+        }
+        
+        [UnityTest]
+        public IEnumerator SetText_Should_ChangeTextContent_When_TextComponentWasConnectedBefore()
+        {
+            AttachTextComponentToDummyGameObject();
+            
+            TargetScript.Text = TextContent;
+            
+            Assert.AreEqual(TextComponent.text, TextContent);
+            yield return null;
+        }
+    }
+}
